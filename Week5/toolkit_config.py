b6d42ec4da0fa302c53421db1d7235d09d64bf99
Merjoex-1 <max1212@foxmail.com>
2023-10-30 00:15:00 +1100

    Initial commit

---
+++ Week5/toolkit_config.py
@@ -0,0 +1,4 @@
+import os
+
+PRJDIR = r"C:\Users\hongy\PycharmProjects\toolkit\Week5"
+DATADIR = os.path.join(PRJDIR, "Week5\data")
---
+++ project1/toolkit_config.py
@@ -0,0 +1,4 @@
+import os
+
+PRJDIR = r"C:\Users\hongy\PycharmProjects\toolkit"
+DATADIR = os.path.join(PRJDIR, "data")
